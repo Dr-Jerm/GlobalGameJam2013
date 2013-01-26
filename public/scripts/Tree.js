8cf6e909c85ddbce6683cda21d8ced01cc914e74
JX <asenox@gmail.com>
2013-01-26 11:42:37 -0700

    tree gen

--- public/scripts/Tree.js
+++ public/scripts/Tree.js
@@ -1,8 +1,10 @@
 function Tree(_point, _rot, _angle)
 {
 
-	this.pos = _point; 
+	this.pos = _point;
 	this.rot = _rot;
 	this.agl = _angle;
-	
+	var index = Math.floor((Math.random()*Assets.tree_limit)+1);
+	var obj = "tree"+index+".mdl.js";
+	var tex = "tree"+index+".jpg";
 }
