0b28e8304214d9ca8c41aebda2ab4d2e530f4e76
e dawg <egeusz@gmail.com>
2013-01-26 03:23:05 -0700

    fuck yeah

--- public/scripts/Game.js
+++ public/scripts/Game.js
@@ -1,25 +1,25 @@
 function Game()
 {
 
-	//THREE.js Variables
-	this.renderer;
-	this.camera;
-	this.scene;
+  this.isPulse; 
+  this.wasPulse; 
+
+  this.player = new Player(); 
+  this.shadowList = new Array();
+  this.treeList = new Array();
 
 
+  this.init = function(){
+    this.setupThree();
+  }
+
+  this.setupThree = function(){
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( this.renderer.domElement );
 
 
-	this.init = function(){
-		this.setupThree();
-	}
-
-	this.setupThree = function(){
-		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-    	this.renderer.setSize( window.innerWidth, window.innerHeight );
-    	document.body.appendChild( this.renderer.domElement );
-
-
-    	this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
+      this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
         this.camera.position.z = 400;
         this.camera.position.y = 20;
 
@@ -27,13 +27,47 @@ function Game()
 
         this.scene = new THREE.Scene();
 
-      	var geometry = new THREE.CubeGeometry( 200, 200, 200 );
-      	var material = new THREE.MeshBasicMaterial( { color: 0xffffff} );
-      	var mesh = new THREE.Mesh( geometry, material );
-      	this.scene.add( mesh );
+        var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+        var material = new THREE.MeshBasicMaterial( { color: 0xffffff} );
+        var mesh = new THREE.Mesh( geometry, material );
+        this.scene.add( mesh );
 
-      	//test function, remove
-      	this.renderer.render(this.scene,this.camera);
+        //test function, remove
+        this.renderer.render(this.scene,this.camera);
 
-	}	
+  } 
+
+
+  this.Update = function()
+  {
+
+    PulseSwitch();
+    this.player.Update();
+  }
+
+
+  var PulseSwitch = function(); 
+  {
+
+    //<   switch logic here  >
+
+    
+    if(isPulse && !wasPulse)// begin pulse  
+    {
+      for(var s in this.shadowList)
+      {
+        this.shadowList[s].Move(); 
+      }
+    }
+    if(!isPulse && wasPulse)// end pulse  
+    {
+     
+    }
+
+  }
+
+
+
+
+  
 }
